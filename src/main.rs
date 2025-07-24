42c13e5573f7b69e9526624a6446dbf239ef2407
nahal <mrym.mhdzde@gmail.com>
2025-07-24 11:30:25 +0330

    Define Block struct and implement timestamp function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Debug)]
+struct Block {
+    index: u64,
+    timestamp: u128,
+    data: String,
+    previous_hash: String,
+    hash: String,
+}
+
+fn get_timestamp() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis()
+}
+
 fn main() {
     println!("Hello, world!");
 }
