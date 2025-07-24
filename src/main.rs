f2821d20c57a9cb3ee649848c2c0d2bc26d82723
nahal <mrym.mhdzde@gmail.com>
2025-07-24 11:49:01 +0330

    Add Blockchain struct with add_block method and test in main

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ struct Block {
     hash: String,
 }
 
+struct Blockchain {
+    blocks: Vec<Block>,
+}
+
+impl Blockchain {
+    fn new() -> Self {
+        let genesis_block = Block::new(0, "Genesis Block".to_string(), "0".to_string());
+        Blockchain {
+            blocks: vec![genesis_block],
+        }
+    }
+
+    fn add_block(&mut self, data: String) {
+        let last_block = self.blocks.last().expect("Blockchain is empty");
+        let new_block = Block::new(last_block.index + 1, data, last_block.hash.clone());
+        self.blocks.push(new_block);
+    }
+}
+
 impl Block {
     fn new(index: u64, data: String, previous_hash: String) -> Self {
         let timestamp = get_timestamp();
@@ -41,6 +60,12 @@ fn calculate_hash(index: u64, timestamp: u128, data: &str, previous_hash: &str) 
 }
 
 fn main() {
-    let genesis_block = Block::new(0, "First Block".to_string(), "0".to_string());
-    println!("{:#?}", genesis_block);
+    let mut blockchain = Blockchain::new();
+
+    blockchain.add_block("First trasaction".to_string());
+    blockchain.add_block("Second trasaction".to_string());
+
+    for block in blockchain.blocks.iter() {
+        println!("{:#?}", block);
+    }
 }
