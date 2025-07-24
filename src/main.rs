d695cf894ccee52950e1bcaba8e0f91e2e6e65f5
nahal <mrym.mhdzde@gmail.com>
2025-07-24 11:41:33 +0330

    Implement Block::new with real hash calculation and create genesis block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sha2::{Digest, Sha256};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug)]
@@ -9,6 +10,21 @@ struct Block {
     hash: String,
 }
 
+impl Block {
+    fn new(index: u64, data: String, previous_hash: String) -> Self {
+        let timestamp = get_timestamp();
+        let hash = calculate_hash(index, timestamp, &data, &previous_hash);
+
+        Block {
+            index,
+            timestamp,
+            data,
+            previous_hash,
+            hash,
+        }
+    }
+}
+
 fn get_timestamp() -> u128 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -16,6 +32,15 @@ fn get_timestamp() -> u128 {
         .as_millis()
 }
 
+fn calculate_hash(index: u64, timestamp: u128, data: &str, previous_hash: &str) -> String {
+    let input = format!("{}{}{}{}", index, timestamp, data, previous_hash);
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    format!("{:x}", result)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let genesis_block = Block::new(0, "First Block".to_string(), "0".to_string());
+    println!("{:#?}", genesis_block);
 }
